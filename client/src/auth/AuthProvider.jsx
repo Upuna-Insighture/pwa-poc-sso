49d9fdbd8ba6326d1715dee8b76b18e807a06040
upuna keerawella <upuna.keerawella_ne@ntucguest.com>
2024-09-23 22:41:48 +0530

    cors test

--- client/src/auth/AuthProvider.jsx
+++ client/src/auth/AuthProvider.jsx
@@ -15,7 +15,6 @@ const AuthProvider = ({ children }) => {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
         },
       })
         .then((response) => {
