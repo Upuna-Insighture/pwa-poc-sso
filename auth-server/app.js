5be583a32c1b89099e6b0cb4c74d5112adb5090e
upuna keerawella <upuna.keerawella_ne@ntucguest.com>
2024-09-24 10:20:47 +0530

    cors test

--- auth-server/app.js
+++ auth-server/app.js
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors({
-  origin: "https://pwa-poc-sso-server.vercel.app", // Allow your client origin
+  origin: 'https://pwa-poc-sso-client.vercel.app', // Allow your client origin
   credentials: true, // Allow credentials (cookies)
   methods: "GET,POST,PUT,DELETE", 
   allowedHeaders: ["Content-Type", "Accept", "Access-Control-Allow-Credentials"], // Specify allowed headers
